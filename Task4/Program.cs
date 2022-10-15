9cbaebf9ff0ff0896de2478405c8a986364d095e
Оля <moskvinaov5@gmail.com>
2022-10-15 19:30:21 +0300

    add Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите три числа поочереди: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > a) max = b;
+if (c > a) max = c;
+Console.Write("максимальное число: " + max);
